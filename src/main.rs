45b5049d0e6346f602209a14a761d00c057a0da3
Robert Garrett <robertgarrett404@gmail.com>
2025-05-22 17:45:57 -0500

    Slapdash API call demo
    I can hit the endpoint and retrieve data. Excellent.

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+
+use reqwest::Error;
+use reqwest::header::{
+    USER_AGENT,
+    ACCEPT
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let request_url = format!(
+        "http:/localhost:3000/api/v1/repos/{owner}/{repo}/releases",
+        owner = "robert",
+        repo = "rcalc",
+    );
+    let client = reqwest::Client::new();
+    let response = client
+        .get(request_url)
+        .header(USER_AGENT, "gt-tools-test-agent")
+        .header(ACCEPT, "application/json")
+        .send()
+        .await?;
+    let body_text: String = response.text().await?;
+
+    println!("{:?}", body_text);
+    Ok(())
+}
