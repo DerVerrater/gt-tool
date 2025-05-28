e19602925e0b95e1623bf8ed9910c542896665cd
Robert Garrett <robertgarrett404@gmail.com>
2025-05-27 23:02:27 -0500

    Add release_attachment module
    For whatever reason, releases and release attachments are separate modules in the Gitea codebase. I'm going to continue mirroring that structure for now.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,3 +1,4 @@
 
 pub mod packages;
 pub mod release;
+pub mod release_attachment;
---
+++ src/api/release_attachment.rs
@@ -0,0 +1,10 @@
+
+pub fn check_release_match_repo(){}
+pub fn get_release_attachment(){}
+pub fn list_release_attachments(){
+    todo!();
+}
+pub fn create_release_attachment(){}
+pub fn edit_release_attachment(){}
+pub fn delete_release_attachment(){}
+
